3c3fcd0a13937c276923296619f5e149b5d8b991
tejasraibagi26 <tejasraibagi@gmail.com>
2020-06-13 02:00:43 +0530

    Resize not allowed added

--- src/Components/MainComponent.js
+++ src/Components/MainComponent.js
@@ -1,17 +1,26 @@
 import React from 'react';
 import Header from './HeaderComponents';
 import TextContainer from './TextFieldComponent';
+import $ from 'jquery';
 // import Details from './Details';
 
-class Main extends React.Component {
-    render() {
-        return (
-            <main>
-                <Header />
-                <TextContainer />
-            </main>
-        );
-    }
+function Main() {
+
+    $(window).on('resize', function() {
+        var $containerHeight = $(window).height();
+        var $containerWidth = $(window).width();
+
+        if($containerHeight <= 1200 || $containerWidth <= 1500){
+            alert('Do not move away from the website');
+        }
+    });
+
+    return (
+        <main>
+            <Header />
+            <TextContainer />
+        </main>
+    );
 }
 
 export default Main;
