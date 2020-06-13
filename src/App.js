12248f911c38925122b8e6bbc4414ff7f87a6ff0
tejasraibagi26 <tejasraibagi@gmail.com>
2020-06-13 20:45:25 +0530

    Disabled Shift + arrowKeys

--- src/App.js
+++ src/App.js
@@ -23,6 +23,18 @@ function App() {
     });
   });
 
+  var ar = new Array(37, 38, 39, 40);
+  $(document).keydown(function (e) {
+    var key = e.which;
+    if ($.inArray(key, ar) > -1 && e.shiftKey) {
+      e.preventDefault();
+      var alertmsg = document.getElementById('alert-msg');
+      alertmsg.innerHTML = "Function Disabled";
+      return false;
+    }
+    return true;
+  });
+
   return (
     <Main />
   );
